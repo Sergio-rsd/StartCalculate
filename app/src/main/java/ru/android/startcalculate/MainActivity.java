ab2e47058b3c9f0c4d7d336c81e44f3a364acb23
Sergio-rsd <sergsa2001@mail.ru>
2021-12-28 00:16:44 +0300

    ДЗ к уроку 5 задание 3 сделано - запуск стороннего приложения

--- app/src/main/java/ru/android/startcalculate/MainActivity.java
+++ app/src/main/java/ru/android/startcalculate/MainActivity.java
@@ -2,13 +2,31 @@ package ru.android.startcalculate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialButton startCalculator = findViewById(R.id.launch_calc);
+
+        startCalculator.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String uri = "ru.android.lesson2";
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        Intent intent = new Intent();
+        intent.setClassName(uri,uri+".MainActivity");
+        startActivity(intent);
     }
 }
